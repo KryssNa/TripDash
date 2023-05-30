0a761c98377bbed21cc42068be90ddd9d7c27bcd
Ayooub Raj <98674950+Ayooubraj@users.noreply.github.com>
2023-05-30 20:01:22 +0545

    add_place
    The initial started

---
+++ add_place.dart
@@ -0,0 +1,7 @@
+sdk.dir=C:\\Users\\Ayooub\\AppData\\Local\\Android\\Sdk
+flutter.sdk=C:\\Src\\flutter
+sdk.dir=C:\\Users\\Ayooub\\AppData\\Local\\Android\\sdk
+flutter.sdk=E:\\flutter
+flutter.buildMode=debug
+flutter.versionName=1.0.0
+flutter.versionCode=1
