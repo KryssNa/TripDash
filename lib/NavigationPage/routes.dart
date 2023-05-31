ba5a449ae47f4687188c8536b9072b69e33002ae
KryssNa <krishna.kryss@gmail.com>
2023-05-31 12:54:41 +0545

    routes for registerScreen added

--- lib/NavigationPage/routes.dart
+++ lib/NavigationPage/routes.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:tripdash/Screens/UserScreen/RegisterScreen.dart';
 
 // We use name route
 // All our routes will be available here
 // you must create a route name in the corresponding screen file to use it here
 // for example : static String routeName = "/register"; in lib\Screens\UserScreen\Signup\register.dart
 final Map<String, WidgetBuilder> routes = {
-  // Register.routeName: (context) => Register(),
+  RegisterScreen.routeName: (context) => const RegisterScreen(),
 
 };
