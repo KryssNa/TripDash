8f1b2e9916d6f26571c9fda2b3ab8c3404cce88c
KryssNa <krishna.kryss@gmail.com>
2023-06-01 07:20:18 +0545

    :art: some color properties improved

--- lib/constant/Colors.dart
+++ lib/constant/Colors.dart
@@ -1,21 +1,25 @@
 import 'package:flutter/material.dart';
 
- class ConstColors {
-  static const Color primaryColor = Color(0xFF0373F3);
-  static const Color secondaryColor = Color(0xFF0C0C0C);
-  static const Color primaryTextColor = Color(0xFF000000);
-  static const Color secondaryTextColor = Color(0xFFB4B4B4);
+class ConstColors {
+ static const Color primaryColor = Color(0xff3e7dc3);
+ static const Color primaryColor2 = Color(0xFF0F4B6F);
+ static const Color secondaryColor = Color(0xFFFF6700);
+ static const Color primaryTextColor = Color(0xB316191D);
+ static const Color secondaryTextColor = Color(0xFFCACCCE);
 
-  static const Color buttonColor = Color(0xFF0373F3);
-  static const Color buttonTextColor = Color(0xFFFFFFFF);
+ static const Color whiteColor = Colors.white;
 
-  static const Color backColor = Color.fromRGBO(191, 134, 143, 30);
-  static const Color iconColor = Color.fromRGBO(0, 0, 0, 1);
+ static const Color buttonColor = Color(0x800079c0);
+ static const Color buttonTextColor = Color(0xFFFFFFFF);
+ static const Color buttonColor2 = Color(0xFF007097);
 
-  static const primaryGradientColor = LinearGradient(
-   begin: Alignment.topLeft,
-   end: Alignment.bottomRight,
-   colors: [Color(0xFFFFA53E), Color(0xFFFF7643)],
-  );
+ static const Color backColor = Color.fromRGBO(191, 134, 143, 30);
+ static const Color iconColor = Color.fromRGBO(0, 0, 0, 1);
+
+ static const primaryGradientColor = LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+  colors: [Color(0xFFFFA53E), Color(0xFFFF7643)],
+ );
 }
 
