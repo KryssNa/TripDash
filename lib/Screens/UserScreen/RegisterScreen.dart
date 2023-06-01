be9f3d31ccb05e7f46cb80c72bace3409f96c11d
KryssNa <krishna.kryss@gmail.com>
2023-06-01 09:35:54 +0545

    register image with title added

--- lib/Screens/UserScreen/RegisterScreen.dart
+++ lib/Screens/UserScreen/RegisterScreen.dart
@@ -20,8 +20,40 @@ class _RegisterScreenState extends State<RegisterScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        color: ConstColors.primaryColor,
-        child: SingleChildScrollView()
+        color: ConstColors.primaryColor2,
+        child: SingleChildScrollView(
+          child: Container(
+            margin: const EdgeInsets.only(top: 40),
+            child: Column(
+              children: [
+                Row(
+
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+
+                  children: [
+
+                    const Text('REGISTER',
+                      style: TextStyle(
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'SSFPro',
+                        letterSpacing: 1.2,
+                        color: ConstColors.whiteColor,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 150,
+                      width: 200,
+                      child:Image.asset('Assets/images/register.png'),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+
+        )
       )
     );
   }
