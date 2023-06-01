0e027aa41e37a5d93419a5a3ff22482526109e9e
biraj011 <biraj.ocg@lebs.edu.np>
2023-06-01 16:57:02 +0545

    made-a-outling

--- lib/Screens/homepage/home_page.dart
+++ lib/Screens/homepage/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../widget/TripPlanWidgets/trip_images.dart';
+
 class Homepage extends StatefulWidget {
   const Homepage({Key? key}) : super(key: key);
   static const routeName = '/Homepage';
@@ -72,9 +74,78 @@ class _HomepageState extends State<Homepage> {
                   prefixIcon: Icon(Icons.search),
                   prefixIconColor:Colors.black,
                 )
-                
               ),
-            )
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                child:Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Column(children: [
+                        TripImages(imageAddress: "https://images.unsplash.com/photo-1592364395653-83e648b20cc2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nzd8fGluZG9uZXNpYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60", edgeInsets: EdgeInsets.only( bottom: 10, right: 15, top: 0, left: 5), values: 185),
+                      ],)
+                    
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text(
+                        "Popular Destination",
+                        style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Column(children: [
+                        TripImages(imageAddress: "https://images.unsplash.com/photo-1592364395653-83e648b20cc2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nzd8fGluZG9uZXNpYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60", edgeInsets: EdgeInsets.only( bottom: 10, right: 15, top: 0, left: 5), values: 185),
+                      ],)
+                    
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text(
+                        "Find the place in the map",
+                        style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Column(children: [
+                        TripImages(imageAddress: "https://images.unsplash.com/photo-1592364395653-83e648b20cc2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nzd8fGluZG9uZXNpYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60", edgeInsets: EdgeInsets.only( bottom: 10, right: 15, top: 0, left: 5), values: 185),
+                      ],)
+                    
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text(
+                        "Recommended",
+                        style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Column(children: [
+                        TripImages(imageAddress: "https://images.unsplash.com/photo-1592364395653-83e648b20cc2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nzd8fGluZG9uZXNpYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60", edgeInsets: EdgeInsets.only( bottom: 10, right: 15, top: 0, left: 5), values: 185),
+                      ],)
+                    
+                    ),
+                  ],
+                ),
+              ),
+              )
+            
       ],
       )
     );
