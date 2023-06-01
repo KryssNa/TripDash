3a2047dbf61520a44afdf5012c0e86716c799caa
KryssNa <krishna.kryss@gmail.com>
2023-06-01 07:22:59 +0545

    :zap: code structure improved

--- lib/constant/constants.dart
+++ lib/constant/constants.dart
@@ -18,26 +18,13 @@ const defaultDuration = Duration(milliseconds: 250);
 // Form Error
 final RegExp emailValidatorRegExp =
 RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
-const String kEmailNullError = "Please Enter your email";
-const String kInvalidEmailError = "Please Enter Valid Email";
-const String kPassNullError = "Please Enter your password";
-const String kShortPassError = "Password is too short";
-const String kMatchPassError = "Passwords don't match";
-const String kNamelNullError = "Please Enter your name";
-const String kPhoneNumberNullError = "Please Enter your phone number";
-const String kAddressNullError = "Please Enter your address";
+const String emailNullError = "Please Enter your email";
+const String invalidEmailError = "Please Enter Valid Email";
+const String passNullError = "Please Enter your password";
+const String shortPassError = "Password is too short";
+const String matchPassError = "Passwords don't match";
+const String namelNullError = "Please Enter your name";
+const String phoneNumberNullError = "Please Enter your phone number";
+const String addressNullError = "Please Enter your address";
 
-final inputDecoration = InputDecoration(
-  contentPadding:
-  EdgeInsets.symmetric(vertical: getProportionateScreenWidth(15)),
-  border: outlineInputBorder(),
-  focusedBorder: outlineInputBorder(),
-  enabledBorder: outlineInputBorder(),
-);
 
-OutlineInputBorder outlineInputBorder() {
-  return OutlineInputBorder(
-    borderRadius: BorderRadius.circular(getProportionateScreenWidth(15)),
-    borderSide: BorderSide(color: ConstColors.primaryColor),
-  );
-}
