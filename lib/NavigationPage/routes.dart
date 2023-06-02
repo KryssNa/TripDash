c0e92f18b480db2c2326cd0e5b13b7f2319ce910
Jenish Budhahtoki <budathokizone111@gmail.com>
2023-06-02 14:56:48 +0545

    added TriplPlan route in routes file

--- lib/NavigationPage/routes.dart
+++ lib/NavigationPage/routes.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:tripdash/Screens/UserScreen/RegisterScreen.dart';
+import 'package:tripdash/Screens/UserScreen/trip_plan.dart';
 
 // We use name route
 // All our routes will be available here
 // you must create a route name in the corresponding screen file to use it here
 // for example : static String routeName = "/register"; in lib\Screens\UserScreen\Signup\register.dart
 final Map<String, WidgetBuilder> routes = {
-  RegisterScreen.routeName: (context) => const RegisterScreen(),
+  RegisterScreen.routeName: (context) => const TripPlan(),
 
 };
