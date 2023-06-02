4f7d53f15ed71c6e0c7ba145d5c24554c9c19976
KryssNa <krishna.kryss@gmail.com>
2023-06-02 13:36:23 +0545

    :art: improve code structure

--- lib/Repositeries/auth_repositeries.dart
+++ lib/Repositeries/auth_repositeries.dart
@@ -1,12 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-
-import '../Services/firebase_service.dart';
 import '../model/user_model.dart';
+import '../services/firebase_service.dart';
 
 class AuthRepository {
-
-
   CollectionReference<UserModel> userRef =
   FirebaseService.db.collection("users").withConverter<UserModel>(
     fromFirestore: (snapshot, _) {
@@ -35,4 +32,44 @@ class AuthRepository {
     }
   }
 
+  Future<UserCredential> login(String email, String password) async {
+    try {
+      UserCredential uc = await FirebaseService.firebaseAuth
+          .signInWithEmailAndPassword(email: email, password: password);
+      return uc;
+    } catch (err) {
+      rethrow;
+    }
+  }
+
+  Future<UserModel> getUserDetail(String id) async {
+    try {
+      final response = await userRef.where("user_id", isEqualTo: id).get();
+
+      var user = response.docs.single.data();
+      user.fcmToken = "";
+      await userRef.doc(user.userId).set(user);
+      return user;
+    } catch (err) {
+      rethrow;
+    }
+  }
+
+  Future<bool> resetPassword(String email) async {
+    try {
+      var res = await FirebaseService.firebaseAuth
+          .sendPasswordResetEmail(email: email);
+      return true;
+    } catch (err) {
+      rethrow;
+    }
+  }
+
+  Future<void> logout() async {
+    try {
+      await FirebaseService.firebaseAuth.signOut();
+    } catch (err) {
+      rethrow;
+    }
+  }
 }
