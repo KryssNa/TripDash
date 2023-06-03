41fbc00098d805de61e4018d8cfa7964a55df393
Jenish Budhahtoki <budathokizone111@gmail.com>
2023-06-03 10:48:35 +0545

    chages the file name trip_plan to trip_plan_overview

--- lib/NavigationPage/routes.dart
+++ lib/NavigationPage/routes.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tripdash/Screens/UserScreen/RegisterScreen.dart';
-import 'package:tripdash/Screens/UserScreen/TripPlan/trip_plan.dart';
+import 'package:tripdash/Screens/UserScreen/TripPlan/trip_plan_overview.dart';
 
 // We use name route
 // All our routes will be available here
