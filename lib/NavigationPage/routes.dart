1032517c0057953ce21635d42e8b555279ebea51
Jenish Budhahtoki <budathokizone111@gmail.com>
2023-06-03 11:19:32 +0545

    changed the route to gitplanoverview

--- lib/NavigationPage/routes.dart
+++ lib/NavigationPage/routes.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tripdash/Screens/UserScreen/RegisterScreen.dart';
+import 'package:tripdash/Screens/UserScreen/TripPlan/trip_plan_detail.dart';
 import 'package:tripdash/Screens/UserScreen/TripPlan/trip_plan_overview.dart';
 
 // We use name route
@@ -7,6 +8,6 @@ import 'package:tripdash/Screens/UserScreen/TripPlan/trip_plan_overview.dart';
 // you must create a route name in the corresponding screen file to use it here
 // for example : static String routeName = "/register"; in lib\Screens\UserScreen\Signup\register.dart
 final Map<String, WidgetBuilder> routes = {
-  RegisterScreen.routeName: (context) => RegisterScreen(),
+  RegisterScreen.routeName: (context) => TripPlanOverview(),
 
 };
