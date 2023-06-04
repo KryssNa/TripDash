a8d7e39713682f62970978f513060b795f3a16bc
KryssNa <krishna.kryss@gmail.com>
2023-06-04 07:35:49 +0545

    :truck: routes of trip plan added

--- lib/NavigationPage/routes.dart
+++ lib/NavigationPage/routes.dart
@@ -4,10 +4,10 @@ import 'package:tripdash/Screens/UserScreen/TripPlan/trip_plan_detail.dart';
 import 'package:tripdash/Screens/UserScreen/TripPlan/trip_plan_overview.dart';
 
 // We use name route
-// All our routes will be available here
-// you must create a route name in the corresponding screen file to use it here
-// for example : static String routeName = "/register"; in lib\Screens\UserScreen\Signup\register.dart
+
 final Map<String, WidgetBuilder> routes = {
-  RegisterScreen.routeName: (context) => TripPlanOverview(),
+  RegisterScreen.routeName: (context) => const RegisterScreen(),
+  TripPlanOverview.routeName: (context) => const TripPlanOverview(),
+  TripPlanDetails.routeName: (context) => const TripPlanDetails(),
 
 };
