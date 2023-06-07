509ef65ac8adabee517c821511a8586a5821e15b
Jenish Budhahtoki <budathokizone111@gmail.com>
2023-06-07 11:09:03 +0545

    had issue in the previous branch so created new one

--- lib/NavigationPage/routes.dart
+++ lib/NavigationPage/routes.dart
@@ -3,11 +3,13 @@ import 'package:tripdash/Screens/UserScreen/RegisterScreen.dart';
 import 'package:tripdash/Screens/UserScreen/TripPlan/trip_plan_detail.dart';
 import 'package:tripdash/Screens/UserScreen/TripPlan/trip_plan_overview.dart';
 
+import '../Screens/Profile/UserProfile.dart';
+
 // We use name route
 // All our routes will be available here
 // you must create a route name in the corresponding screen file to use it here
 // for example : static String routeName = "/register"; in lib\Screens\UserScreen\Signup\register.dart
 final Map<String, WidgetBuilder> routes = {
-  RegisterScreen.routeName: (context) => TripPlanOverview(),
+  RegisterScreen.routeName: (context) => UserProfile(),
 
 };
