2e371eedad44ece9fb038f2a57a264c4535ddb2c
haxor09alek <alekh.ocg@lebs.edu.np>
2023-06-08 11:57:59 +0545

    made new changes

--- lib/login.dart
+++ lib/login.dart
@@ -1,12 +1,20 @@
+import 'package:flutter/cupertino.dart';
+
 class login extends StatefulWidget {
+  static var routeName;
+
   const login({super.key});
 
   @override
   State<login> createState() => _loginState();
 }
 
+// ignore: camel_case_types
 class _loginState extends State<login> {
   @override
+  // ignore: no_logic_in_create_state   
+  // ignore: unused_field
+  
   Widget build(BuildContext context) {
     return Container();
   }
