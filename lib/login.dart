af95cf406d876fda18e56ffa82d3102015e781c8
haxor09alek <alekh.ocg@lebs.edu.np>
2023-06-08 11:19:59 +0545

    new widget

---
+++ lib/login.dart
@@ -0,0 +1,13 @@
+class login extends StatefulWidget {
+  const login({super.key});
+
+  @override
+  State<login> createState() => _loginState();
+}
+
+class _loginState extends State<login> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
