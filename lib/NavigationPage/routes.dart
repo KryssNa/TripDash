73904ba7a168dc3aa84b2e552cd72d0cd7bc2e2f
haxor09alek <alekh.ocg@lebs.edu.np>
2023-06-08 13:28:51 +0545

    new push

--- lib/NavigationPage/routes.dart
+++ lib/NavigationPage/routes.dart
@@ -1,7 +1,10 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
 import 'package:tripdash/Screens/UserScreen/RegisterScreen.dart';
-import 'package:tripdash/Screens/UserScreen/TripPan/trip_plan_detail.dart';
-import 'package:tripdash/Screens/UserScreen/TripPlan/trip_plan_overview.dart';
+//import 'package:tripdash/Screens/UserScreen/TripPan/trip_plan_detail.dart';
+//import 'package:tripdash/Screens/UserScreen/TripPlan/trip_plan_overview.dart';
+import 'package:tripdash/login.dart';
 
 // We use name route
 
@@ -9,5 +12,6 @@ final Map<String, WidgetBuilder> routes = {
   RegisterScreen.routeName: (context) => const RegisterScreen(),
   TripPlanOverview.routeName: (context) => const TripPlanOverview(),
   TripPlanDetails.routeName: (context) => const TripPlanDetails(),
+  login.routeName: (context) =>const login();
 
 };
