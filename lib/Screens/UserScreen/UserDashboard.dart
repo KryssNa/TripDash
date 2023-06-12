eb5beb56fd575466efc1c4e2300377b820b3d15c
KryssNa <krishna.kryss@gmail.com>
2023-06-12 16:24:34 +0545

    :tada: started working on userDashboard

---
+++ lib/Screens/UserScreen/UserDashboard.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class UserDashboard extends StatefulWidget {
+  const UserDashboard({super.key});
+
+  @override
+  State<UserDashboard> createState() => _UserDashboardState();
+}
+
+class _UserDashboardState extends State<UserDashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
