c78c1f6e56aa5b5aeaebb826cc03bdb49f7ecdc3
biraj011 <biraj.ocg@lebs.edu.np>
2023-06-12 10:03:08 +0545

    started a work

---
+++ lib/Screens/AdminScreen/eventcreation.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Admineventcreation extends StatefulWidget {
+  
+  const Admineventcreation({super.key});
+  static const routeName = '/Admineventcreation';
+
+  @override
+  State<Admineventcreation> createState() => _AdmineventcreationState();
+}
+
+class _AdmineventcreationState extends State<Admineventcreation> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
