e680d2323b53b9a21f3ae3847a565bf946dc3d7b
biraj011 <biraj.ocg@lebs.edu.np>
2023-06-18 19:09:51 +0545

    resolve the file name pattern

--- lib/Repositeries/EventRepositories.dart
+++ lib/Repositeries/EventRepositories.dart
@@ -1,6 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-import '../model/Event_Model.dart';
+import '../model/EventModel.dart';
 import '../services/firebase_service.dart';
 
 class EventRepository{
