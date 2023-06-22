69e67836702b6bbce17d09fb693898f4f9992742
KryssNa <krishna.kryss@gmail.com>
2023-06-22 13:53:52 +0545

    :white_check_mark: BottomNavigationBar completed

--- lib/widget/BottomNavigationBar.dart
+++ lib/widget/BottomNavigationBar.dart
@@ -17,16 +17,63 @@ class BottomNavigationBarWidget extends StatefulWidget {
 
 class _BottomNavigationBarWidgetState extends State<BottomNavigationBarWidget> {
   int index = 0;
-  GlobalKey<CurvedNavigationBarState> _bottomNavigationKey = GlobalKey();
-
-  late PageController _pageController;
 
   void initState() {
     super.initState();
-    _pageController = PageController();
   }
 
   @override
   Widget build(BuildContext context) {
-
+    return Scaffold(
+      bottomNavigationBar: CurvedNavigationBar(
+        color: Color(0xfff1eded),
+        buttonBackgroundColor: Color(0xffe7f3f1),
+        items: [ CurvedNavigationBarItem(
+          child: Icon(Icons.home_outlined,color: (index == 0)? Colors.green : Colors.black,size: 30,),
+          label: 'Home',
+        ),
+          CurvedNavigationBarItem(
+            child: Icon(Icons.search,color: (index == 1)? Colors.green : Colors.black,size: 30),
+            label: 'Search',
+          ),
+          CurvedNavigationBarItem(
+            child: Icon(Icons.perm_identity,color: (index == 2)? Colors.green : Colors.black,size: 30),
+            label: 'Personal',
+          )],
+        index: index,
+        onTap: (selctedIndex){
+          setState(() {
+            index = selctedIndex;
+          });
+        },
+        height: 60,
+        backgroundColor: Colors.transparent,
+        animationDuration: const Duration(milliseconds: 500),
+      ),
+      body: Container(
+          color: Colors.blue,
+          width: double.infinity,
+          height: double.infinity,
+          alignment: Alignment.center,
+          child: getSelectedWidget(index: index)
+      ),
+    );
   }
+
+  Widget getSelectedWidget({required int index}){
+    Widget widget;
+    switch(index){
+      case 0:
+        widget = const UserDashboard();
+        break;
+      case 1:
+        widget = const Homepage();
+        break;
+      default:
+        widget = const UserProfile();
+        break;
+    }
+    return widget;
+  }
+
+}
