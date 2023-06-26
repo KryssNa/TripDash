1b6dcc9f041a59f3b21040266b87b19bee45c746
haxor09alert <chaudharyalekh@gmail.com>
2023-06-26 20:08:39 +0545

    started new feature

---
+++ lib/booking.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+    
+class Booking extends StatefulWidget {
+  const Booking({Key? key}) : super(key: key);
+
+  @override
+  _BookingState createState() => _BookingState();
+}
+
+class _BookingState extends State<Booking> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(''),
+      ),
+      body: Container(),
+    );
+  }
+}
