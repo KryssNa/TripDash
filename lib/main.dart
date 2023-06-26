467e6fc268f579d36642b36968051a6572c5e4fa
haxor09alert <chaudharyalekh@gmail.com>
2023-06-26 19:54:08 +0545

    added

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:tripdash/Screens/AdminScreen/addhotel.dart';
 import 'package:tripdash/Screens/Hotel_view/hotel_home_screen.dart';
 import 'Helper/multiProvider.dart';
 import 'NavigationPage/routes.dart';
