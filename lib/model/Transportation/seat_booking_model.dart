528d9f05ffe06df3fe164ba10fc4bc76e1b129af
KryssNa <krishna.kryss@gmail.com>
2023-06-27 22:00:32 +0545

    jason from snapshot

--- lib/model/Transportation/seat_booking_model.dart
+++ lib/model/Transportation/seat_booking_model.dart
@@ -21,4 +21,31 @@ class SeatBookingModel {
     required this.seatNumbers,
   });
 
+  factory SeatBookingModel.fromSnapshot(DocumentSnapshot snapshot) {
+    final data = snapshot.data() as Map<String, dynamic>;
+    return SeatBookingModel(
+      userId: data['userId'] ?? '',
+      transactionId: data['transactionId'] ?? '',
+      total: data['total'] ?? 0,
+      noOfTickets: data['noOfTickets'] ?? '',
+      sourceLocation: data['sourceLocation'] ?? '',
+      destinationLocation: data['destinationLocation'] ?? '',
+      date: data['date'] ?? '',
+      seatNumbers: List<String>.from(data['seatNumbers']) ?? [],
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'userId': userId,
+      'transactionId': transactionId,
+      'total': total,
+      'noOfTickets': noOfTickets,
+      'sourceLocation': sourceLocation,
+      'destinationLocation': destinationLocation,
+      'date': date,
+      'seatNumbers': seatNumbers,
+    };
+  }
+
 }
