ebc794c2b1853c916d8b1b768d189b0f3c89e85d
biraj011 <biraj.ocg@lebs.edu.np>
2023-06-30 12:32:56 +0545

    intial changes

---
+++ lib/Screens/UserScreen/Event/ViewEvents.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import '../app_bar.dart';
+import '../user_app_drawer.dart';
+
+class ViewEvents extends StatefulWidget {
+  static const routeName = '/ViewEvents';
+  const ViewEvents({super.key});
+
+  @override
+  State<ViewEvents> createState() => _ViewEventsState();
+}
+
+class _ViewEventsState extends State<ViewEvents> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: const UserAppDrawer(),
+      appBar: const PreferredSize(
+        preferredSize: Size.fromHeight(50), // Change the height as desired
+        child: AppBarWidget(avatar:'Assets/avatars/av_1.png'),
+    )
+    );
+  }
+}
