4e8447aa165ba312ee2f2175bf833b8b7b654187
KryssNa <krishna.kryss@gmail.com>
2023-06-30 11:01:09 +0545

    view details of places

---
+++ lib/Screens/Transportation/view_details.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:tripdash/Screens/Transportation/choose_transportation.dart';
+import 'package:tripdash/constant/colors.dart';
+import 'package:tripdash/constant/fonts.dart';
+import 'Widgets/custom_button.dart';
+import 'Widgets/interest_item.dart';
+import 'Widgets/photo_item.dart';
+
+class DetailPage extends StatelessWidget {
+  const DetailPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Widget backgroundImage() {
+      return Container(
+        width: double.infinity,
+        height: 450,
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage('assets/image_destination1.png'))),
+      );
+    }
+
+ 
+
+
+
+    return Scaffold(
+      backgroundColor: ConstColors.kBackgroundColor,
+      body: SingleChildScrollView(
+        child: Stack(
+          children: [backgroundImage(), customShadow(), content()],
+        ),
+      ),
+    );
+  }
+}
