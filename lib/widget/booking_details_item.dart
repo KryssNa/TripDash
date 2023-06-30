281c436e6c4ded3b3c0bced691056c89d8eef1d6
haxor09alek <alekh.ocg@lebs.edu.np>
2023-06-30 07:37:38 +0545

    New widgets

---
+++ lib/widget/booking_details_item.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+// ignore: depend_on_referenced_packages
+import 'package:aeroplane/shared/theme.dart';
+
+class BookingDetailsItem extends StatelessWidget {
+  final String title;
+  final String valueText;
+  final Color valueColor;
+
+  const BookingDetailsItem({
+    Key? key,
+    required this.title,
+    required this.valueText,
+    required this.valueColor,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 16),
+      child: Row(children: [
+        Container(
+          width: 16,
+          height: 16,
+          margin: EdgeInsets.only(right: 6),
+          decoration: BoxDecoration(
+            image: DecorationImage(image: AssetImage('assets/icon_check.png')),
+          ),
+        ),
+        Text(title, style: blackTextStyle),
+        const Spacer(),
+        Text(
+          valueText,
+          style:
+              blackTextStyle.copyWith(fontWeight: semiBold, color: valueColor),
+        )
+      ]),
+    );
+  }
+}
