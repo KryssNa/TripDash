2053b9241317bae180783a32b3c6f0b197be20f6
ayooubraj <ayooubr.g11@gmail.com>
2023-06-30 21:17:29 +0545

    ViewPlace

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:tripdash/Screens/AdminScreen/add_place.dart';
-import 'package:tripdash/Screens/ViewPlace/place_screen.dart';
-import 'package:tripdash/widget/bottom_navigation_bar.dart';
+
 import 'Helper/multi_provider.dart';
 import 'NavigationPage/routes.dart';
 
