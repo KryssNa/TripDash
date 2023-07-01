2f6d0b27c5e4b92e6efa9dc874e78138ad1f68aa
KryssNa <krishna.kryss@gmail.com>
2023-07-01 09:51:26 +0545

    routes added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:tripdash/Screens/AdminScreen/add_place.dart';
-import 'package:tripdash/Screens/auth/register_screen.dart';
-
+import 'package:tripdash/Screens/splash_screen.dart';
 import 'Helper/multi_provider.dart';
 import 'NavigationPage/routes.dart';
 
@@ -30,7 +28,7 @@ class _MyAppState extends State<MyApp> {
       child: GetMaterialApp(
         navigatorKey: navigatorKey,
         debugShowCheckedModeBanner: false,
-        initialRoute: RegisterScreen.routeName,
+        initialRoute: SplashPage.routeName,
         routes: routes,
       ),
     );
