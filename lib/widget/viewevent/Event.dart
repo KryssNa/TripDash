f1fbb5a575caa0009940b43732702f0fc1abacaa
biraj011 <biraj.ocg@lebs.edu.np>
2023-07-01 12:46:09 +0545

    changes for viewing events

---
+++ lib/widget/viewevent/Event.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class PackageContainer extends StatelessWidget {
+  final String category;
+  final String date;
+  final String location;
+  final String description;
+  final String imageUrl;
+  final String title;
+
+
+  const PackageContainer({Key? key, required this.title, required this.description, required this.location, required this.date, required this.imageUrl, required this.category}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 150,
+      width: 525,
+      margin:
+      const EdgeInsets.only(left: 15, right: 15, top: 10),
+      child: Card(
+        elevation: 5,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        
+      ),
+    );
+  }
+}
