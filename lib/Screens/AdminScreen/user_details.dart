4603ff2a25096c590b3290b09889e7093e2f3cbe
Jenish Budhahtoki <budathokizone111@gmail.com>
2023-07-03 16:25:25 +0545

    did some work on user details

--- lib/Screens/AdminScreen/user_details.dart
+++ lib/Screens/AdminScreen/user_details.dart
@@ -11,7 +11,95 @@ class _UserDetailsState extends State<UserDetails> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(
+        // title: Text('${userTitle()} ${user.firstName} ${user.lastName}'),
+      ),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 16.0,
+            ),
+            Center(
+              child: Hero(
+                tag: 1,
+                child: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      "https://images.unsplash.com/photo-1518002054494-3a6f94352e9d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGthdGhtYW5kdXxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80"),
+                  radius: 100.0,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 22.0,
+            ),
+            Text(
+              'Hi Man',
+              style: TextStyle(
+                fontSize: 25.0,
+                fontWeight: FontWeight.w700,
+              ),
+              textAlign: TextAlign.left,
+            ),
+            SizedBox(
+              height: 12.0,
+            ),
+            Text(
+              'Paley Dai',
+              style: TextStyle(
+                fontSize: 18.0,
+                fontWeight: FontWeight.w500,
+              ),
+              textAlign: TextAlign.left,
+            ),
+            SizedBox(
+              height: 12.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text(
+                  'PaleDai@gmail.com',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w300,
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    // customLaunch(
+                    //     'mailto:${user.email}?subject=Contact%20Information&body=Type%20your%20mail%20here');
+                  },
+                  child: Icon(
+                    Icons.email,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 12.0,
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                      left: 12.0, right: 12.0, bottom: 20.0),
+                  child: Text(
+                    "hi",
+                    textAlign: TextAlign.justify,
+                    style: TextStyle(
+                      fontSize: 16.0,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
