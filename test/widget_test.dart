7e2b13ebdda55ce3dfeb34ac1256dd431ddefb4b
KryssNa <krishna.kryss@gmail.com>
2023-07-03 10:30:22 +0545

    error solved

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,8 +1,6 @@
 
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:tripdash/main.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
