571777cbdb03c93c6b514b8f02d0499d90b735ca
KryssNa <krishna.kryss@gmail.com>
2023-07-03 10:27:52 +0545

    test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,7 +6,7 @@ import 'package:tripdash/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget( const MyApp());
+    // await tester.pumpWidget( const MyApp());
 
     // Verify that our counter starts at 0.
 
