79e6b5a6bf538b1fe02bda7cd238b1957c49b5f8
Jenish Budhahtoki <budathokizone111@gmail.com>
2023-07-03 15:54:36 +0545

    started work on UserDetails

---
+++ lib/Screens/AdminScreen/user_details.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class UserDetails extends StatefulWidget {
+  const UserDetails({Key? key}) : super(key: key);
+
+  @override
+  State<UserDetails> createState() => _UserDetailsState();
+}
+
+class _UserDetailsState extends State<UserDetails> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
