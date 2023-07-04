d8979fae054384781648538db3ac32ce9aa64f3e
Libanad <libanadhikari33@gmail.com>
2023-07-04 21:21:46 +0545

    created textfield and button

--- lib/Screens/UserScreen/UpdatePassword.dart
+++ lib/Screens/UserScreen/UpdatePassword.dart
@@ -2,15 +2,124 @@ import 'package:flutter/material.dart';
 
 class UpdatePassword extends StatefulWidget {
   static const routeName = '/UpdatePassword';
-  const UpdatePassword({super.key});
+
+  const UpdatePassword({Key? key}) : super(key: key);
 
   @override
-  State<UpdatePassword> createState() => _UpdatePasswordState();
+  _UpdatePasswordState createState() => _UpdatePasswordState();
 }
 
 class _UpdatePasswordState extends State<UpdatePassword> {
+  final TextEditingController _currentPasswordController = TextEditingController();
+  final TextEditingController _newPasswordController = TextEditingController();
+  final TextEditingController _confirmPasswordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _currentPasswordController.dispose();
+    _newPasswordController.dispose();
+    _confirmPasswordController.dispose();
+    super.dispose();
+  }
+
+  void _updatePassword() {
+    String currentPassword = _currentPasswordController.text;
+    String newPassword = _newPasswordController.text;
+    String confirmPassword = _confirmPasswordController.text;
+
+    // Validate input
+    if (currentPassword.isEmpty || newPassword.isEmpty || confirmPassword.isEmpty) {
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('Error'),
+            content: Text('Please fill in all fields.'),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+      return;
+    }
+
+    if (newPassword != confirmPassword) {
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('Error'),
+            content: Text('New password and confirm password do not match.'),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+      return;
+    }
+
+    // TODO: Implement password update logic here
+    // You can perform the necessary actions to update the password,
+    // such as making API requests or updating the database.
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Success'),
+          content: Text('Password updated successfully.'),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Update Password'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _currentPasswordController,
+              decoration: InputDecoration(labelText: 'Current Password'),
+              obscureText: true,
+            ),
+            TextField(
+              controller: _newPasswordController,
+              decoration: InputDecoration(labelText: 'New Password'),
+              obscureText: true,
+            ),
+            TextField(
+              controller: _confirmPasswordController,
+              decoration: InputDecoration(labelText: 'Confirm Password'),
+              obscureText: true,
+            ),
+            SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: _updatePassword,
+              child: Text('Update Password'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
