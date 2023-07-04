36517f7bbfbab27d02851c621fe4fbf573bb4500
Libanad <libanadhikari33@gmail.com>
2023-07-04 08:03:41 +0545

    started work on Update Password

---
+++ lib/Screens/UserScreen/UpdatePassword.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class UpdatePassword extends StatefulWidget {
+  const UpdatePassword({super.key});
+
+  @override
+  State<UpdatePassword> createState() => _UpdatePasswordState();
+}
+
+class _UpdatePasswordState extends State<UpdatePassword> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
