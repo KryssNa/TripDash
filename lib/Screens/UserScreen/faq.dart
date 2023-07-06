23b82ec0e33173d3dd0434b71622abcb5fe558e4
Jenish Budhahtoki <budathokizone111@gmail.com>
2023-07-06 21:49:30 +0545

    started work on FAQ

---
+++ lib/Screens/UserScreen/faq.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FAQ extends StatefulWidget {
+  const FAQ({Key? key}) : super(key: key);
+
+  @override
+  State<FAQ> createState() => _FAQState();
+}
+
+class _FAQState extends State<FAQ> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
