5b2d6ccc0cf05b9cd4f041b93c2bd01f7d9c580d
biraj011 <biraj.ocg@lebs.edu.np>
2023-07-07 19:03:25 +0545

    small changes

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:intl/date_symbol_data_local.dart';
-import 'package:tripdash/Screens/AdminScreen/add_events.dart';
 import 'package:tripdash/Screens/UserScreen/Event/view_events.dart';
-import 'package:tripdash/Screens/UserScreen/user_dashboard.dart';
 import 'Helper/multi_provider.dart';
 import 'NavigationPage/routes.dart';
 
