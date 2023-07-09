f697152a62b57d18c751786b7e3b63172214d7ac
Jenish Budhahtoki <budathokizone111@gmail.com>
2023-07-09 15:15:27 +0545

    work completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:tripdash/Screens/AdminScreen/CustomerDetail/customer_detail.dart';
 import 'package:tripdash/Screens/splash_screen.dart';
 import 'Helper/multi_provider.dart';
 import 'NavigationPage/routes.dart';
@@ -32,7 +31,7 @@ class _MyAppState extends State<MyApp> {
       child: GetMaterialApp(
         navigatorKey: navigatorKey,
         debugShowCheckedModeBanner: false,
-        initialRoute: CustomerDetail.routeName,
+        initialRoute: SplashPage.routeName,
         routes: routes,
       ),
     );
