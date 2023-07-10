2f6464f5889a1902d3d77a9550822dc542bffab0
ayooubraj <ayooubr.g11@gmail.com>
2023-07-10 21:22:31 +0545

    Error resloved

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,7 @@
 
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:tripdash/main.dart';
+
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
