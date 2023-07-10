b807d918c215ca39f8e50796ed4526b65a8ee805
Jenish Budhahtoki <budathokizone111@gmail.com>
2023-07-10 17:09:40 +0545

    started work on Account screen

--- lib/NavigationPage/routes.dart
+++ lib/NavigationPage/routes.dart
@@ -17,6 +17,7 @@ import 'package:tripdash/Screens/TransportationScreen/choose_transportation.dart
 import 'package:tripdash/Screens/TransportationScreen/my_bookings.dart';
 import 'package:tripdash/Screens/TransportationScreen/view_details.dart';
 import 'package:tripdash/Screens/UserScreen/AboutUs/about_us.dart';
+import 'package:tripdash/Screens/UserScreen/Account/Account.dart';
 
 import 'package:tripdash/Screens/UserScreen/forgot_password.dart';
 
@@ -68,4 +69,5 @@ final Map<String, WidgetBuilder> routes = {
   HotelHomeScreen.routeName: (context) => const HotelHomeScreen(),
   FAQ.routeName: (context) => const FAQ(),
   CustomerDetail.routeName: (context) => const CustomerDetail(),
+  Account.routeName: (context) => const Account(),
 };
