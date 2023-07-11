f8b04e87a355af0ba7f433f754d8f8720b7f4dc8
KryssNa <krishna.kryss@gmail.com>
2023-07-11 20:28:17 +0545

    :white_check_mark: final push routes for payment added

--- lib/NavigationPage/routes.dart
+++ lib/NavigationPage/routes.dart
@@ -17,6 +17,7 @@ import 'package:tripdash/Screens/TransportationScreen/choose_transportation.dart
 import 'package:tripdash/Screens/TransportationScreen/my_bookings.dart';
 import 'package:tripdash/Screens/TransportationScreen/view_details.dart';
 import 'package:tripdash/Screens/UserScreen/AboutUs/about_us.dart';
+import 'package:tripdash/Screens/UserScreen/account_balance.dart';
 
 import 'package:tripdash/Screens/UserScreen/forgot_password.dart';
 
@@ -70,4 +71,5 @@ final Map<String, WidgetBuilder> routes = {
   FAQ.routeName: (context) => const FAQ(),
   CustomerDetail.routeName: (context) => const CustomerDetail(),
   PaymentScreen.routeName: (context) => const PaymentScreen(),
+  AccountBalance.routeName:(context)=>const AccountBalance(),
 };
