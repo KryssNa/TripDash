1161db77719ae9689e29df9bddcba05e4533c15e
KryssNa <krishna.kryss@gmail.com>
2023-07-12 20:54:10 +0545

    function to fetch userDetails

---
+++ lib/Screens/AdminScreen/admin_transaction.dart
@@ -0,0 +1,41 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class AdminTransactionScreen extends StatefulWidget {
+  const AdminTransactionScreen({super.key});
+
+  @override
+  State<AdminTransactionScreen> createState() => _AdminTransactionScreenState();
+}
+
+class _AdminTransactionScreenState extends State<AdminTransactionScreen> {
+
+  CollectionReference usersCollection =
+  FirebaseFirestore.instance.collection('users');
+  List<DocumentSnapshot> userDocuments = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchUsers();
+  }
+
+  Future<void> fetchUsers() async {
+    try {
+      QuerySnapshot querySnapshot = await usersCollection.get();
+      setState(() {
+        userDocuments = querySnapshot.docs;
+      });
+    } catch (error) {
+      if (kDebugMode) {
+        print('Error retrieving users: $error');
+      }
+      // Handle the error gracefully
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
