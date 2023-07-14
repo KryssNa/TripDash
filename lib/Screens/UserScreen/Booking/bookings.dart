c965deb870f89fe66bc44745f1e9bca48c842ebd
Jenish Budhahtoki <budathokizone111@gmail.com>
2023-07-14 11:13:37 +0545

    created a booking page

--- lib/Screens/UserScreen/Booking/bookings.dart
+++ lib/Screens/UserScreen/Booking/bookings.dart
@@ -10,6 +10,85 @@ class Bookings extends StatefulWidget {
 class _BookingsState extends State<Bookings> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return MaterialApp(title: 'Bookings Page',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Bookings Page'),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              BookingCard(
+                title: 'Hotel Booking',
+                description: 'Book a hotel for your stay',
+                onTap: () {
+                  // Handle hotel booking tap
+                },
+              ),
+              BookingCard(
+                title: 'Place Booking',
+                description: 'Book a place to visit',
+                onTap: () {
+                  // Handle place booking tap
+                },
+              ),
+              BookingCard(
+                title: 'Package Booking',
+                description: 'Book a vacation package',
+                onTap: () {
+                  // Handle package booking tap
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class BookingCard extends StatelessWidget {
+  final String title;
+  final String description;
+  final VoidCallback onTap;
+
+  const BookingCard({
+    required this.title,
+    required this.description,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Card(
+        margin: EdgeInsets.all(16.0),
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                title,
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 8.0),
+              Text(
+                description,
+                style: TextStyle(fontSize: 16.0),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
