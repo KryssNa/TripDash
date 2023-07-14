13bcd99f084330147aa61db4ca9eb0e6c8ff9666
Jenish Budhahtoki <budathokizone111@gmail.com>
2023-07-14 11:01:15 +0545

    started work on bookings

---
+++ lib/Screens/UserScreen/Booking/Bookings.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Bookings extends StatefulWidget {
+  const Bookings({Key? key}) : super(key: key);
+
+  @override
+  State<Bookings> createState() => _BookingsState();
+}
+
+class _BookingsState extends State<Bookings> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
