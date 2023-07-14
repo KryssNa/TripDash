a49cbea81b74c42f3e4cc7547b204c9d4b9811ef
Jenish Budhahtoki <budathokizone111@gmail.com>
2023-07-14 12:19:59 +0545

    started work on AirplaneBooking

---
+++ lib/Screens/UserScreen/Booking/airplaneBooking.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AirplaneBooking extends StatefulWidget {
+  const AirplaneBooking({Key? key}) : super(key: key);
+
+  @override
+  State<AirplaneBooking> createState() => _AirplaneBookingState();
+}
+
+class _AirplaneBookingState extends State<AirplaneBooking> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
