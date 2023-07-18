7bd93649a6c0f7748e898e6595ccc96da810f0cc
Jenish Budhahtoki <budathokizone111@gmail.com>
2023-07-18 15:25:18 +0545

    started work on AdminBookingPlace

---
+++ lib/Screens/AdminScreen/AdminBookingPlace.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AdminBookingPlace extends StatefulWidget {
+  const AdminBookingPlace({Key? key}) : super(key: key);
+
+  @override
+  State<AdminBookingPlace> createState() => _AdminBookingPlaceState();
+}
+
+class _AdminBookingPlaceState extends State<AdminBookingPlace> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
