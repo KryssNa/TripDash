20f894999cbb8387e3059436e3080c0185360d29
Jenish Budhahtoki <budathokizone111@gmail.com>
2023-07-20 12:29:26 +0545

    addeed database to the code

--- lib/Screens/AdminScreen/AdminBookingPlace.dart
+++ lib/Screens/AdminScreen/AdminBookingPlace.dart
@@ -1,5 +1,5 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class AdminBookingPlace extends StatefulWidget {
   const AdminBookingPlace({Key? key}) : super(key: key);
@@ -9,244 +9,60 @@ class AdminBookingPlace extends StatefulWidget {
   State<AdminBookingPlace> createState() => _AdminBookingPlaceState();
 }
 
-class _AdminBookingPlaceState extends State<AdminBookingPlace>
-    with SingleTickerProviderStateMixin {
-
-
-  late AnimationController _animationController;
-  late Animation<double> _opacityAnimation;
-  late Animation<double> _scaleAnimation;
-
-  // Sample list of bookings
-  List<Map<String, dynamic>> bookings = [
-    {
-      'id': '1',
-      'user': 'John Doe',
-      'place': 'Place A',
-      'date': '2023-07-18',
-    },
-    {
-      'id': '2',
-      'user': 'Jane Smith',
-      'place': 'Place B',
-      'date': '2023-07-19',
-    },
-    {
-      'id': '3',
-      'user': 'Bob Johnson',
-      'place': 'Place C',
-      'date': '2023-07-20',
-    },
-  ];
-
-  @override
-  void initState() {
-    _animationController = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 500),
-    );
-
-    _opacityAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
-      CurvedAnimation(
-        parent: _animationController,
-        curve: Curves.easeInOut,
-      ),
-    );
-
-    _scaleAnimation = Tween<double>(begin: 0.5, end: 1.0).animate(
-      CurvedAnimation(
-        parent: _animationController,
-        curve: Curves.easeInOut,
-      ),
-    );
-
-    super.initState();
-  }
-
-  Future<List<Map<String, dynamic>>> getAirplaneBookings() async {
-    QuerySnapshot snapshot = await FirebaseFirestore.instance
-        .collection('AirplaneBooking')
-        .get();
-    return snapshot.docs.map((doc) => doc.data() as Map<String, dynamic>).toList();
-  }
-
-  Future<List<Map<String, dynamic>>> getBusBookings() async {
-    QuerySnapshot snapshot =
-    await FirebaseFirestore.instance.collection('BusBooking').get();
-    return snapshot.docs.map((doc) => doc.data() as Map<String, dynamic>).toList();
-  }
-  Future<List<Map<String, dynamic>>> getPackageBookings() async {
-    QuerySnapshot snapshot = await FirebaseFirestore.instance
-        .collection('PackageBooking')
-        .get();
-    return snapshot.docs.map((doc) => doc.data() as Map<String, dynamic>).toList();
-  }
-
-  Future<List<Map<String, dynamic>>> getHotelBookings() async {
-    QuerySnapshot snapshot =
-    await FirebaseFirestore.instance.collection('HotelBooking').get();
-    return snapshot.docs.map((doc) => doc.data() as Map<String, dynamic>).toList();
-  }
-
-  Future<List<Map<String, dynamic>>> getPlaceBookings() async {
-    QuerySnapshot snapshot =
-    await FirebaseFirestore.instance.collection('PlaceBooking').get();
-    return snapshot.docs.map((doc) => doc.data() as Map<String, dynamic>).toList();
-  }
-
-
-  @override
-  void dispose() {
-    _animationController.dispose();
-    super.dispose();
-  }
-
-  Future<Future<bool?>> _showDeleteConfirmationDialog(int index) async {
-    return showDialog<bool>(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Confirm Deletion'),
-          content: Text('Are you sure you want to delete this booking?'),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop(false); // Return false for cancel
-              },
-              child: Text('Cancel'),
-            ),
-            TextButton(
-              onPressed: () {
-                setState(() {
-                  bookings.removeAt(index);
-                });
-                Navigator.of(context).pop(true); // Return true for delete
-              },
-              child: Text('Delete'),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
+class _AdminBookingPlaceState extends State<AdminBookingPlace> {
   @override
   Widget build(BuildContext context) {
-    _animationController.forward();
-
     return Scaffold(
       appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-        ),
-        title: const Text('Admin Bookings'),
-        flexibleSpace: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [Colors.blue, Colors.green], // Add your preferred colors here
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-        ),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.refresh),
-            onPressed: () {
-              // Add the refresh logic here
-            },
-          ),
-        ],
+        title: Text('Admin Booking Places'),
       ),
-      body: AnimatedBuilder(
-        animation: _animationController,
-        builder: (context, child) {
-          return Opacity(
-            opacity: _opacityAnimation.value,
-            child: Transform.scale(
-              scale: _scaleAnimation.value,
-              child: bookings.isEmpty // Display a loading indicator while the list is empty
-                  ? Center(child: CircularProgressIndicator())
-                  : ListView.builder(
-                itemCount: bookings.length,
-                itemBuilder: (BuildContext context, int index) {
-                  final booking = bookings[index];
-                  return Dismissible(
-                    key: Key(booking['id']),
-                    direction: DismissDirection.endToStart,
-                    confirmDismiss: (direction) async {
-                      return await _showDeleteConfirmationDialog(index);
-                    },
-                    onDismissed: (direction) {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(
-                          content: Text('Booking deleted.'),
-                          action: SnackBarAction(
-                            label: 'UNDO',
-                            onPressed: () {
-                              setState(() {
-                                bookings.insert(index, booking);
-                              });
-                            },
-                          ),
-                        ),
-                      );
-                    },
-                    background: Container(
-                      color: Colors.red,
-                      alignment: Alignment.centerRight,
-                      padding: EdgeInsets.symmetric(horizontal: 16.0),
-                      child: Icon(
-                        Icons.delete,
-                        color: Colors.white,
-                      ),
-                    ),
-                    child: Card(
-                      elevation: 4.0,
-                      margin: EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
-                      child: ListTile(
-                        title: Text(
-                          'Booking ID: ${booking['id']}',
-                          style: TextStyle(
-                            fontSize: 16.0,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        subtitle: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              'User: ${booking['user']}',
-                              style: TextStyle(
-                                fontSize: 14.0,
-                                fontWeight: FontWeight.normal,
-                              ),
-                            ),
-                            Text(
-                              'Place: ${booking['place']}',
-                              style: TextStyle(
-                                fontSize: 14.0,
-                                fontWeight: FontWeight.normal,
-                              ),
-                            ),
-                            Text(
-                              'Date: ${booking['date']}',
-                              style: TextStyle(
-                                fontSize: 14.0,
-                                fontWeight: FontWeight.normal,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  );
-                },
-              ),
-            ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection('Bookings').snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          // Data is ready, so let's process and display it.
+          final bookingDocs = snapshot.data?.docs; // Use the null-aware operator here
+
+          if (bookingDocs == null || bookingDocs.isEmpty) {
+            return Center(
+              child: Text('No data available'),
+            );
+          }
+
+          return ListView.builder(
+            itemCount: bookingDocs.length,
+            itemBuilder: (context, index) {
+              var bookingData = bookingDocs[index].data() as Map<String, dynamic>; // Cast to Map<String, dynamic>
+              String name = bookingData['name'] ?? ''; // Use null-aware operator and provide a default value
+              String place = bookingData['place'] ?? '';
+              String package = bookingData['package'] ?? '';
+              String bus = bookingData['bus'] ?? '';
+              String aeroplane = bookingData['aeroplane'] ?? '';
+              String hotel = bookingData['hotel'] ?? '';
+
+              return ListTile(
+                title: Text('Name: $name'),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Place: $place'),
+                    Text('Package: $package'),
+                    Text('Bus: $bus'),
+                    Text('Aeroplane: $aeroplane'),
+                    Text('Hotel: $hotel'),
+                  ],
+                ),
+              );
+            },
           );
         },
       ),
