839cae7e46a415e6eb2731b8570fb559b4f46dc3
KryssNa <krishna.kryss@gmail.com>
2023-07-21 21:43:32 +0545

    initial commit

---
+++ lib/Screens/term_and_condition.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class TermAndConditionScreen extends StatelessWidget {
+  const TermAndConditionScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
