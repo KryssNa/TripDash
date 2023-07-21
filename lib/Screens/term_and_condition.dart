974584482e23378612a1df5222f10195fd0dccf3
KryssNa <krishna.kryss@gmail.com>
2023-07-21 21:45:43 +0545

    adding more details

--- lib/Screens/term_and_condition.dart
+++ lib/Screens/term_and_condition.dart
@@ -1,11 +1,46 @@
 
 import 'package:flutter/material.dart';
 
-class TermAndConditionScreen extends StatelessWidget {
-  const TermAndConditionScreen({super.key});
+class TermsAndConditionsScreen extends StatelessWidget {
+  const TermsAndConditionsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Terms and Conditions'),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Terms and Conditions for TripDash',
+              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              // Place the entire Terms and Conditions text here
+              'By accessing and using TripDash, you acknowledge ...',
+              style: TextStyle(fontSize: 16.0),
+            ),
+            SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Back'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
