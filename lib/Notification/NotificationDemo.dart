edb37a5a6a16dac9d750a502115e2bfcf92395a5
biraj011 <biraj.ocg@lebs.edu.np>
2023-07-22 14:25:36 +0545

    notificationdemo

--- lib/Notification/NotificationDemo.dart
+++ lib/Notification/NotificationDemo.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'NotificationService.dart';
+
 class NotificationDemo extends StatefulWidget {
   const NotificationDemo({super.key});
   static String routeName = "/NotificationDemo";
@@ -11,6 +13,23 @@ class NotificationDemo extends StatefulWidget {
 class _NotificationDemoState extends State<NotificationDemo> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return return Scaffold(
+      appBar: AppBar(
+        title: Text("Notification Demo"),
+      ),
+      body: Column(
+        children: [
+          ElevatedButton(
+              onPressed: () {
+                NotificationService.display(
+                    title: "Notification Title",
+                    body: "Notification body",
+                    image: "assets/images/logo.png",
+                    logo: "assets/images/logo.png");
+              },
+              child: Text("Send Notification"))
+        ],
+      ),
+    );
   }
 }
