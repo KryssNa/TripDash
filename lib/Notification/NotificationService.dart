d6ad038d284834569862163ce6bf7d70f1a47d1c
biraj011 <biraj.ocg@lebs.edu.np>
2023-07-22 14:32:32 +0545

    notification services

--- lib/Notification/NotificationService.dart
+++ lib/Notification/NotificationService.dart
@@ -5,5 +5,44 @@ class NotificationService {
   static final FlutterLocalNotificationsPlugin _notificationsPlugin =
       FlutterLocalNotificationsPlugin();
 
+static Future<void> display(
+      {required String title,
+      required String body,
+      String? payload,
+      BuildContext? buildContext,
+      String? image,
+      String? logo}) async {
+    if (buildContext != null) {
+      context = buildContext;
+    }
 
+    // if image from asset
+    var styleinformationDesign;
+    if (image != null && logo != null) {
+      late AndroidBitmap<Object>? notificationImage;
+      late AndroidBitmap<Object>? notificationLogo;
+      try {
+        var imageLoader = await getImageFilePathFromAssets(image, 'bigpicture');
+        notificationImage = FilePathAndroidBitmap(imageLoader);
+        var imageLoaderLogo = await getImageFilePathFromAssets(logo, 'smallpicture');
+        notificationLogo = FilePathAndroidBitmap(imageLoaderLogo);
+        styleinformationDesign = BigPictureStyleInformation(
+          notificationImage,
+          largeIcon: notificationLogo,
+        );
+      } catch (e) {
+        print("Notification Error ${e}");
+      }
+    }
+
+    final id = DateTime.now().millisecondsSinceEpoch ~/ 1000;
+    final NotificationDetails notificationDetails = NotificationDetails(
+        android: AndroidNotificationDetails("myapp", "myapp channel",
+            channelDescription: "Channel Description",
+            importance: Importance.max,
+            priority: Priority.high,
+            styleInformation: styleinformationDesign),
+        iOS: DarwinNotificationDetails());
+    _notificationsPlugin.show(id, title, body, notificationDetails, payload: payload);
+  }
 }
