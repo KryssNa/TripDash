7e5ef23de50c4ee30803cf011ed8d685d1b3e514
biraj011 <biraj.ocg@lebs.edu.np>
2023-07-22 14:18:59 +0545

    notifications

--- lib/Notification/NotificationDemo.dart
+++ lib/Notification/NotificationDemo.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-
-import 'NotificationService.dart';
 
 class NotificationDemo extends StatefulWidget {
-  const NotificationDemo({Key? key}) : super(key: key);
-   static const routeName = '/NotificationDemo';
+  const NotificationDemo({super.key});
 
   @override
   State<NotificationDemo> createState() => _NotificationDemoState();
@@ -14,23 +10,6 @@ class NotificationDemo extends StatefulWidget {
 class _NotificationDemoState extends State<NotificationDemo> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Notification"),
-      ),
-      body: Column(
-        children: [
-          ElevatedButton(
-              onPressed: () {
-                NotificationService.display(
-                    title: "WELCOME USER",
-                    body: "You have successfully login ",
-                    image: "assets/images/logo.png",
-                    logo: "assets/images/logo.png");
-              },
-              child: Text("YOU ARE sUCCESSFULL LOGIN "))
-        ],
-      ),
-    );
+    return const Placeholder();
   }
 }
