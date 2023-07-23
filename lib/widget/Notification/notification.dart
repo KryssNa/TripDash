f08ccdc5ac2d58585327fc92afcc2255ef2eb205
Jenish Budhahtoki <budathokizone111@gmail.com>
2023-07-23 12:45:15 +0545

    started work on notification list page

---
+++ lib/widget/Notification/notification.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NotificationsList extends StatelessWidget {
+  const NotificationsList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
