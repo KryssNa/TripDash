790d06098e92c02c501865888d0960dd16af9838
Jenish Budhahtoki <budathokizone111@gmail.com>
2023-07-23 16:51:08 +0545

    removed unsused import

--- lib/Screens/UserScreen/Profile/user_profile.dart
+++ lib/Screens/UserScreen/Profile/user_profile.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:tripdash/Screens/UserScreen/Profile/update_user_profile.dart';
-import 'package:tripdash/Screens/UserScreen/update_password.dart';
 import 'package:tripdash/widget/UserProfile/user_detail.dart';
 
 class UserProfile extends StatefulWidget {
