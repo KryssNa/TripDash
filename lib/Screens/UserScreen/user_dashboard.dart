7ede937f9514fa351c8dc7886c9c8db5c5124dea
Jenish Budhahtoki <budathokizone111@gmail.com>
2023-07-30 13:12:30 +0545

    removed any errors

--- lib/Screens/UserScreen/user_dashboard.dart
+++ lib/Screens/UserScreen/user_dashboard.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:tripdash/Screens/UserScreen/ViewDetails/view_details2.dart';
 import 'package:tripdash/Screens/UserScreen/ViewDetails/view_details3.dart';
