d35ba1a019fd8dbf0738ee85b02c3232de8d7388
Jenish Budhahtoki <budathokizone111@gmail.com>
2023-07-30 23:32:26 +0545

    started booking page as the previous one was lost

--- lib/Screens/UserScreen/Booking/bookings.dart
+++ lib/Screens/UserScreen/Booking/bookings.dart
@@ -1,32 +1,20 @@
 import 'package:flutter/material.dart';
 
-class UserBookings extends StatelessWidget {
+class UserBookings extends StatefulWidget {
   static String routeName = "/UserBookings";
-  const UserBookings({super.key});
+  const UserBookings({Key? key}) : super(key: key);
+
+  @override
+  State<UserBookings> createState() => _UserBookingsState();
+}
+
+class _UserBookingsState extends State<UserBookings> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Bookings',
-          style: TextStyle(
-            fontFamily: 'Roboto',
-            fontWeight: FontWeight.bold,
-            fontSize: 20.0,
-          ),
-        ),
-        centerTitle: true,
-        elevation: 4.0,
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.pop(context); // Go back to the previous screen
-          },
-        ),
-        backgroundColor: const Color(0xff3e7dc3),
-      ),
-    );
+    return const Placeholder();
   }
 }
+
+
 
 
