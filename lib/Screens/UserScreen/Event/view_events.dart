97bef54cbcda369a6435dadbc95c784a60c0012c
biraj011 <biraj.ocg@lebs.edu.np>
2023-08-01 23:13:59 +0545

    done

--- lib/Screens/UserScreen/Event/view_events.dart
+++ lib/Screens/UserScreen/Event/view_events.dart
@@ -2,8 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:tripdash/widget/event.dart';
 
-import '../app_bar.dart';
-import '../user_app_drawer.dart';
 
 class ViewEvents extends StatefulWidget {
   static const routeName = '/ViewEvents';
